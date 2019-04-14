527d93b02588dd8ef91e9fa3a2c74095d04ccb6d
arinkarus <arinkarus@gmail.com>
2019-04-14 16:57:10 +0300

    Added queue task.

---
+++ Queue.Tests/QueueTests.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Queue;
+
+namespace Queue.Tests
+{
+    public class QueueTests
+    { 
+        [Test]
+        public void CreateQueue_NegativeCapacity_ThrowArgumentException() =>
+           Assert.Throws<ArgumentException>(() => new Queue.Queue<int>(-10));
+
+        [Test]
+        public void CreateQueue_CollectionIsNull_ThrowArgumentNullException() =>
+            Assert.Throws<ArgumentNullException>(() => new Queue.Queue<int>(null));
+
+        [Test]
+        public void CreateQueue_Collection_IncreaseCountProperty()
+        {
+            var list = new List<string>() { "hello", "world" };
+            var queue = new Queue<string>(list);
+            Assert.AreEqual(list.Count, queue.Count);
+        }
+
+        [Test]
+        public void Enqueue_TwoElementGiven_ReturnCount()
+        {
+            var queue = new Queue<int>();
+            queue.Enqueue(5);
+            queue.Enqueue(1111);
+            Assert.AreEqual(2, queue.Count);
+        }
+
+        [Test]      
+        public void Dequeue_QueueIsEmpty_ThrowsInvalidOperationException()
+        {
+            var queue = new Queue<int>();
+            Assert.Throws<InvalidOperationException>(() => queue.Dequeue());
+        }
+
+        [Test]
+        public void Peek_QueueIsEmpty_ThrowsInvalidOperationException()
+        {
+            var queue = new Queue<int>();
+            Assert.Throws<InvalidOperationException>(() => queue.Peek());
+        }
+
+        [Test]
+        public void Peek_QueueHasElements_ReturnElement()
+        {
+            var queue = new Queue<char>();
+            queue.Enqueue('a');
+            queue.Enqueue('b');
+            queue.Enqueue('c');
+            char element = queue.Peek();
+            Assert.IsTrue(element == 'a' && queue.Count == 3);
+        }
+        
+        [Test]
+        public void Dequeue_QueueHasElements_ReturnAndRemoveElement()
+        {
+            var queue = new Queue<char>();
+            queue.Enqueue('a');
+            queue.Enqueue('b');
+            queue.Enqueue('c');
+            char element = queue.Dequeue();
+            Assert.IsTrue(element == 'a' && queue.Count == 2);
+        }
+
+        [Test]
+        public void Foreach_QueueHasElements_ReturnElements()
+        {
+            var result = new List<int>();
+            var queue = new Queue<char>();
+            queue.Enqueue('a');
+            queue.Enqueue('b');
+            queue.Enqueue('c');
+            foreach(var symbol in queue)
+            {
+                result.Add(symbol);
+            }
+            CollectionAssert.AreEqual(new List<char>() { 'a', 'b', 'c' },
+                result);
+        }
+
+        [Test]
+        public void IsEmpty_QueueIsEmtpy_ReturnTrue()
+        {
+            var queue = new Queue<int>();
+            Assert.IsTrue(queue.IsEmpty);
+        }
+            
+
+    }
+}
---
+++ Queue/Queue.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Queue
+{
+    public class Queue<T> : IEnumerable<T>, IEnumerable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue{T}" /> class.
+        /// </summary>
+        public Queue()
+        {
+            this.Elements = new T[this.Capacity];
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue{T}" /> class.
+        /// </summary>
+        public Queue(IEnumerable<T> collection)
+        {
+            this.CheckCollectionOnNull(collection);
+            var list = new List<T>(collection);
+            Elements = list.ToArray();
+            Count = Elements.Length;
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Queue{T}" /> class.
+        /// </summary>
+        public Queue(int capacity)
+        {
+            this.Capacity = capacity;
+            Elements = new T[Capacity];
+        }
+
+        /// <summary>
+        /// Queue capacity.
+        /// </summary>
+        private int capacity;
+
+        /// <summary>
+        /// Gets and sets capacity.
+        /// </summary>
+        private int Capacity
+        {
+            get
+            {
+                return this.capacity;
+            }
+
+            set
+            {
+                CheckCapacity(value);
+                capacity = value;
+            }
+        }
+
+        /// <summary>
+        /// Count of elements in queue.
+        /// </summary>
+        public int Count { get; private set; }
+
+        /// <summary>
+        /// Elements in queue.
+        /// </summary>
+        private T[] Elements { get; set; }
+
+        /// <summary>
+        /// Front index.
+        /// </summary>
+        public int FrontIndex { get; private set; }
+
+        /// <summary>
+        /// Gets Back index.
+        /// </summary>
+        public int BackIndex
+        {
+            get { return (FrontIndex + Count) % Capacity; }
+        }
+
+        /// <summary>
+        /// Gets value that determines if queue is empty.
+        /// </summary>
+        public bool IsEmpty
+        {
+            get
+            {
+                return this.Count == 0;
+            }
+        }
+
+        /// <summary>
+        /// Add item to queue.
+        /// </summary>
+        /// <param name="element">Element i add.</param>
+        public void Enqueue(T element)
+        {
+            if (this.Count == Capacity)
+            {
+                this.IncreaseCapacity();
+            }
+
+            this.Elements[BackIndex] = element;
+            this.Count++;
+        }
+
+        /// <summary>
+        /// Returns item from queue and removes it from queue.
+        /// </summary>
+        /// <returns>Item from queue.</returns>
+        public T Dequeue()
+        {
+            this.CheckForEmptiness();
+            T element = this.Elements[FrontIndex];
+            this.Elements[FrontIndex] = default(T);
+            this.Count--;
+            this.FrontIndex = (FrontIndex + 1) % Capacity;
+            return element;
+        }
+
+        /// <summary>
+        /// Returns item from queue.
+        /// </summary>
+        /// <returns>Item from queue.</returns>
+        public T Peek()
+        {
+            this.CheckForEmptiness();
+            return this.Elements[FrontIndex];
+        }
+
+        /// <summary>
+        /// Gets enumerator.
+        /// </summary>
+        /// <returns>Returns enumerator.</returns>
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < this.Count; i++)
+            {
+                yield return Elements[i];
+            }
+        }
+
+        private void IncreaseCapacity()
+        {
+            this.Capacity++;
+            this.Capacity *= 2;
+            Queue<T> tempQueue = new Queue<T>(this.Capacity);
+            while (this.Count > 0)
+            {
+                tempQueue.Enqueue(this.Dequeue());
+            }
+
+            this.Elements = tempQueue.Elements;
+            this.Count = tempQueue.Count;
+            this.FrontIndex = tempQueue.FrontIndex;
+        }
+
+        private void CheckCapacity(int capacity)
+        {
+            if (capacity < 0)
+            {
+                throw new ArgumentException($"{nameof(capacity)} cant't be a negative number!");
+            }
+        }
+
+        private void CheckForEmptiness()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Queue is empty!");
+            }
+        }
+
+        private void CheckCollectionOnNull(IEnumerable<T> collection)
+        {
+            if (collection == null)
+            {
+                throw new ArgumentNullException($"{nameof(collection)} is null, but collection is required!");
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+}
